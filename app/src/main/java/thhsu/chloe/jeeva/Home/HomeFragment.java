6d788b60d1bbbc5df8856a4b61fddcb2e8b7cd94
Chloe Hsu <chloe.thhsu@gmail.com>
2018-05-01 16:12:13 +0800

    Implemented buttonNavigation functionality.Able to switch to home page and SignInTabPage in MVP pattern. Implemented profile Ui.

--- app/src/main/java/thhsu/chloe/jeeva/Home/HomeFragment.java
+++ app/src/main/java/thhsu/chloe/jeeva/Home/HomeFragment.java
@@ -1,40 +1,38 @@
-package thhsu.chloe.jeeva.SignInTab;
+package thhsu.chloe.jeeva.Home;
 
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import thhsu.chloe.jeeva.R;
 
 /**
- * Created by Chloe on 4/30/2018.
+ * Created by Chloe on 5/1/2018.
  */
 
-public class SignInTabFragment extends Fragment implements SignInTabContract.View {
+public class HomeFragment extends Fragment implements HomeContract.View {
 
-    SignInTabContract.Presenter mPresenter;
+    private HomeContract.Presenter mPresenter;
 
-    public static SignInTabFragment newInstance(){return new SignInTabFragment();}
+
+    public static HomeFragment newInstance(){
+        return new HomeFragment();
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_sign_in_tab, container, false);
-//        ((Button) root.findViewById(R.id.signin_tab_signup_btn));
-//        ((Button) root.findViewById(R.id.signin_tab_signin_btn));
-
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
         return root;
     }
 
     @Override
-    public void setPresenter(SignInTabContract.Presenter presenter) {
+    public void setPresenter(HomeContract.Presenter presenter) {
         mPresenter = presenter;
     }
 
--- app/src/main/java/thhsu/chloe/jeeva/SignInTab/SignInTabPresenter.java
+++ app/src/main/java/thhsu/chloe/jeeva/SignInTab/SignInTabPresenter.java
@@ -1,8 +1,28 @@
 package thhsu.chloe.jeeva.SignInTab;
 
+import thhsu.chloe.jeeva.Home.HomeContract;
+import thhsu.chloe.jeeva.SavedJobs.SavedJobsPresenter;
+
 /**
  * Created by Chloe on 4/30/2018.
  */
 
-public class SignInTabPresenter {
+public class SignInTabPresenter implements SignInTabContract.Presenter {
+
+    private SignInTabContract.View mSignInTabView;
+
+    public SignInTabPresenter(SignInTabContract.View signInTabView){
+        mSignInTabView = signInTabView;
+        if(signInTabView != null){
+            mSignInTabView.setPresenter(this);
+        }
+    }
+
+    @Override
+    public void start() {
+    }
+
+    @Override
+    public void result(int requestCode, int resultCode) {
+    }
 }
