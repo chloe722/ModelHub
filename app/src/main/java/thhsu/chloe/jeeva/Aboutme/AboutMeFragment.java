1699d828c061bb2e45cdb2bad27dfb5c2aa196c8
Chloe Hsu <chloe.thhsu@gmail.com>
2018-05-03 18:03:31 +0800

    Added about page layout and about activity class

---
+++ app/src/main/java/thhsu/chloe/jeeva/Aboutme/AboutMeFragment.java
@@ -0,0 +1,10 @@
+package thhsu.chloe.jeeva.Aboutme;
+
+import android.app.Fragment;
+
+/**
+ * Created by Chloe on 5/3/2018.
+ */
+
+public class AboutMeFragment extends Fragment {
+}
---
+++ app/src/main/java/thhsu/chloe/jeeva/activities/AboutpageActivity.java
@@ -0,0 +1,19 @@
+package thhsu.chloe.jeeva.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import thhsu.chloe.jeeva.R;
+
+/**
+ * Created by Chloe on 5/3/2018.
+ */
+
+public class AboutpageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_page);
+    }
+}
